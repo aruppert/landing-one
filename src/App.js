679d5fc7ca392afe6c1456b93f612d4ae51d1869
Alexander Ruppert <aruppert@pm.me>
2020-11-08 15:14:48 +0100

    Fix halfing of screen

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import GlobalStyles from './GlobalStyles';
 import Quizzme from './components/Quizzme';
 import MiriamExnerDesign from './components/MiriamExnerDesign';
+import MiddleButtons from './components/MiddleButtons';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Quizzme />
+      <MiddleButtons />
       <MiriamExnerDesign />
     </>
   );
